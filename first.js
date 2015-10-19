9856c2859d49b3d73c48af011d90dc3d2c5df213
Basava NavyaSri <basava@thoughtworks.com>
2015-10-19 19:00:26 +0530

    included go to hell

--- first.js
+++ first.js
@@ -2,7 +2,13 @@ var say_hello_world = function(){
 	console.log('hello world');
 };
 say_hello_world();
+
 var say_hai_world = function(){
 	console.log('hai world');
 };
 say_hai_world();
+
+var go_to_hell = function(){
+	console.log('what the hell you are saying stop it.')
+};
+go_to_hell();
